99c47878c44e8f00b4940f1a05e3eb0e2344454a
jschnapper <jschnapper@github.com>
2023-01-02 16:18:43 -0600

    next-auth progress

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       </Head>
       <main>
         <h1>readit</h1>
+        <Link href='/login'>Login</Link>
       </main>
     </>
   )
